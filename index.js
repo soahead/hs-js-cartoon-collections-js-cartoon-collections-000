38f86b266ca6c98adb8742384636214be853ffe4
Adrian Head <soahead@gmail.com>
2020-03-05 17:54:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+
 function dwarfRollCall(dwarves) {
+  let str = ""
+    //loops array members
+   for (let i = 0; i < dwarves.length; i++){
+     if (i < dwarves.length - 1){
+  
+       //display the order of array members
+        str += `${i+1}. ${dwarves[i]} `  ;
+     }
+        //displays the last array member
+      else {
+        str += `${i+1}. ${dwarves[i]} `  ;
+      }
+   }
+return str
 }
 
 function summonCaptainPlanet(planeteerCalls){
